1ef94797ba507f99d78b08ee0c07137a873c5819
Booligo <57365834+Booligo@users.noreply.github.com>
2020-08-04 15:26:04 +0300

    Delete hashes.js

--- hashes.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-  function hashes(){
-    const password_hashes = fs.readFileSync("./pass-hash/passwords.txt","utf8");
-    let hash =[];
-    hash.push(password_hashes.split(/\r?\n/).filter(function(x){return x}).map(function(x){return x}));
-    return  hash[0];
-}
-module.exports.hashes = hashes;
