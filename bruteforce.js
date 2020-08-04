1eb719d0dc44630f1bad816c7bee7e8880951773
Booligo <57365834+Booligo@users.noreply.github.com>
2020-08-04 15:27:40 +0300

    Add files via upload

--- bruteforce.js
+++ bruteforce.js
@@ -1,24 +1,24 @@
 const md5 = require('md5');
 const salt = require('./routes/salt');
-const password = require('./routes/possible_password');
-const hash = require('./routes/hashes');
-const hashes = hash.hashes();
-const pass = password.password();
+const password = require('./routes/helper');
+const hash = require('./routes/helper');
+const hashes = hash.get_data_of_file("pass-hash/passwords.txt");
+const pass = password.get_data_of_file("./pass-hash/dictionary.txt");
 const sol = salt.get_salt();
 function crack() {
     let start = (new Date()).getTime();
     let count = 1;
-        for (let j = 0; j < hashes.length; j++) {
-            let k=0;
-            for (let i = 0; i < pass.length; i++) {
-                count++;
-                    if (md5(`${sol[k]}$${pass[i]}`) === hashes[j]) {
-                    console.log(`password plaintext:${sol[k]}$${pass[i]} = hash:${hashes[j]}`);
-                    break;
-                    }
-                    if(k<sol.length){
-                        k++;
-                    }else k=0;
+    for (let j = 0; j < hashes.length; j++) {
+        let k=0;
+        for (let i = 0; i < pass.length; i++) {
+            count++;
+            if (md5(`${sol[k]}$${pass[i]}`) === hashes[j]) {
+                console.log(`password plaintext:${sol[k]}$${pass[i]} = hash:${hashes[j]}`);
+                break;
+            }
+            if(k<sol.length){
+                k++;
+            }else k=0;
         }
     }
     console.log('time - ',((new Date()) * 1) - start,'ms');
